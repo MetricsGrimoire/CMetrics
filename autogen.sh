057af4b1b85faf054d42332efd58e26c46d6e30a
Carlos Garcia Campos <carlosgc@gsyc.escet.urjc.es>
2007-05-04 14:24:25 +0000

    Remove generated files.
    2007-05-04  Carlos Garcia Campos  <carlosgc@gsyc.escet.urjc.es>
    	* mkinstalldirs:
    	* depcomp:
    	* INSTALL:
    	* missing:
    	* install-sh:
    	Remove generated files. 
    	* common/cmts-autogen.sh:
    	* autogen.sh:
    	Add autogen script.

---
+++ autogen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="cmetrics"
+
+(test -f $srcdir/configure.ac \
+    && test -f $srcdir/ChangeLog \
+) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+. $srcdir/common/cmts-autogen.sh
