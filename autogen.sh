01070af3473f7114d33f354ea9816f5a1d1769c0
Israel Herraiz <israel.herraiz@gmail.com>
2011-03-03 18:15:34 +0100

    Fixing broken scripts if ChangeLog absent

--- autogen.sh
+++ autogen.sh
@@ -5,9 +5,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="cmetrics"
 
-(test -f $srcdir/configure.ac \
-    && test -f $srcdir/ChangeLog \
-) || {
+(test -f $srcdir/configure.ac ) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
